6bda978afaf9f54e338bed96e36e74a46c33411b
Chris McKenzie <me@christophermckenzie.com>
2021-02-05 09:51:10 -0500

    start init

--- src/App.js
+++ src/App.js
@@ -1,98 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  useQuery,
-  gql,
-} from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://graphql-weather-api.herokuapp.com",
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          localHello: {
-            read(_, { variables }) {
-              const { subject } = variables;
-              return `Hello ${subject ? subject : "World"}`
-            },
-          },
-        },
-      },
-    },
-  }),
-});
-
-const LOCAL_HELLO = gql`
-  query localHello($subject: String) {
-    localHello(subject: $subject) @client
-  }
-`;
-
-const GET_WEATHER = gql`
-  query GetWeather($name: String!) {
-    getCityByName(name: $name) {
-      id
-      name
-      weather {
-        wind {
-          speed
-        }
-        clouds {
-          humidity
-          all
-        }
-        temperature {
-          feelsLike
-          actual
-        }
-      }
-    }
-  }
-`;
 
 const WeatherInfo = (props) => {
-  const { loading, error, data } = useQuery(GET_WEATHER, {
-    variables: { name: "New York" },
-  });
-
-  if (loading) {
-    return <div>Loading!!!</div>;
-  }
-
-  const { weather, name } = data.getCityByName;
-
-  return (
-    <div>
-      <h1>Weather for {name}</h1>
-      <p>Temperature: {weather.temperature.actual}</p>
-      <p>Wind: {weather.wind.speed}</p>
-      <p>Cloud cover: {weather.clouds.all}</p>
-    </div>
-  );
+  return null;
 };
 
 const SayHello = (props) => {
-  const { loading, error, data } = useQuery(LOCAL_HELLO, {
-    variables: { subject: "New York" },
-  });
-
-  if (loading) return null;
-
-  return <div style={{ backgroundColor: "red"}}>{data.localHello}</div>;
+  return "Hello";
 };
 
 function App() {
   return (
-    <ApolloProvider client={client}>
       <div className="App">
         <WeatherInfo />
         <SayHello />
       </div>
-    </ApolloProvider>
   );
 }
 
